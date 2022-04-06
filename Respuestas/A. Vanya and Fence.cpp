6eeadfc13e675c4f7df3bda937aabc26735ba38a
Jaimedev12 <A00833173@itesm.mx>
2022-04-05 21:58:23 -0500

    Create A. Vanya and Fence.cpp

---
+++ Respuestas/A. Vanya and Fence.cpp
@@ -0,0 +1,36 @@
+
+// A. Vanya and Fence 
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+void initializeVector(vector<int> &x, int n){
+    for (int i = 0; i < n; i++){
+        cin >> x[i];
+    }
+}
+
+int countWidth(vector<int> a, int n, int h){
+    w = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] <= h)
+            w++;
+        else
+            w += 2;
+    }
+    return w;
+}
+
+int main(){
+    int n, h;
+    cin >> n >> h;
+    
+    // Create a vector the size of the number of friends
+    vector<int> a(n);
+    initializeVector(a, n);
+    
+    int w = countWidth(a, n, h);
+    cout << w;
+}
+
