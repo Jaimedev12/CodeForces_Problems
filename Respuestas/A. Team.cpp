eba00544ebf7ee837f2f82ff496d8e240f49deda
Jaimedev12 <A00833173@itesm.mx>
2022-04-06 21:20:06 -0500

    Update A. Team.cpp

--- Respuestas/A. Team.cpp
+++ Respuestas/A. Team.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 void countSureProblems(int n, int &cnt){
     vector<int> v(3);
-    for (int i = 0; i < n, i++){
+    for (int i = 0; i < n; i++){
         cin >> v[0] >> v[1] >> v[2];
         
         if (v[0] + v[1] + v[2] >= 2){
-            cnt++
+            cnt++;
         }
     }
 }
@@ -19,9 +19,9 @@ void countSureProblems(int n, int &cnt){
 int main(){
     int n = 0, cnt = 0;
     
-    cin >> n
+    cin >> n;
     
-    countSureProblems(n, cnt)
+    countSureProblems(n, cnt);
     
     cout << cnt;
 }
