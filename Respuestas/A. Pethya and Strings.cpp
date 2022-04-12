570abf446c4b869dd7f51b6276460ad564c893a7
Jaimedev12 <A00833173@itesm.mx>
2022-04-12 16:00:53 -0500

    Create A. Pethya and Strings.cpp

---
+++ Respuestas/A. Pethya and Strings.cpp
@@ -0,0 +1,30 @@
+// A. Pethya and Strings
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void lowerCase(string &s){
+    for (int i = 0; i < s.length(); i++){
+        s[i] = tolower(s[i]);
+    }
+}
+
+int main()
+{
+    string string1, string2;
+    cin >> string1 >> string2;
+    vector<char> temp;
+
+    lowerCase(string1);
+    lowerCase(string2);
+    
+    if (string1 == string2){
+        cout << 0;
+    } else if (string1 < string2){
+        cout << -1;
+    } else{
+        cout << 1;
+    }
+}
